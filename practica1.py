027d54f4b53fb240bb919aae321566a6798f8ab8
PIRIS RUIZ <x442898@santanderglobaltech.com>
2021-10-04 16:55:11 +0200

    botones

--- practica1.py
+++ practica1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pygame, sys
+import math
 from pygame.locals import *
 BLACK  = (0, 0,0)
 WHITE  = (255, 255, 255)
@@ -8,10 +9,32 @@ YELLOW = (255, 255, 0)
 BLUE   = (0,0,255)
 GREEN = (0,255,0)
 
+
+
 class Cors:
     def __init__(self,x,y):
         self.x = x
         self.y = y
+
+class Button:
+    def __init__(self,surface,pos,width,height,text):
+        self.surface = surface
+        self.pos = pos
+        self.width = width
+        self.height = height
+        self.text = text
+        self.font = pygame.font.SysFont('Comic Sans MS', 20)
+        
+    def isClicked(self,pos):
+        if (self.pos.x <= pos.x and (self.pos.x + self.width) > pos.x):
+            if (self.pos.y <= pos.y and (self.pos.y + self.height) > pos.y):
+                return True
+        return False
+
+    def draw(self,color):
+        pygame.draw.rect(self.surface,color,pygame.Rect((self.pos.x,self.pos.y),(self.width,self.height)),1)
+        textsurface = self.font.render(self.text, False, (0, 0, 0))
+        surface.blit(textsurface,(self.pos.x  + 5,self.pos.y))
 
 class Square:
     def __init__(self,x,y,cell_size):
@@ -20,20 +43,83 @@ class Square:
         self.cell_size = cell_size
 
 
-def slope_intercept(x1,x2,y1,y2):
+def slope_intercept(x1,y1,x2,y2):
     L = []
+    if x2<x1:
+        x1,y1,x2,y2 = x2,y2,x1,y1
     x = x1
     y = y1
     dx = x2-x1
     dy = y2-y1
-    m = dy/dx
+    if dx != 0:
+        if (dy/dx)>1:
+            x,y = y,x
+            m = 1 / (dy/dx)
+        else:
+            m = dy/dx
     b = y1 - m *x1
     while(x<=x2):
-        L.append(Cors(x,y))
+        if y1 == y2:
+            L.append(Cors(x,y1))
+        if x1 == x2:
+            L.append(Cors(x1,y))
+        else:
+            L.append(Cors(x,y))
+            y = round(m*x+b)
         x = x+1
-        y = round(m*x+b)
     return L
 
+def digital_differential_analyzer(x1,y1,x2,y2):
+    dx = x2-x1
+    dy = y2-y1
+    m =   max([abs(dx),abs(dy)])  
+    dx2 = dx / m
+    dy2 = dy / m
+    x = x1 + 0.5
+    y = y1 + 0.5
+    i = 0
+    L = []
+    while i < m:
+        L.append(Cors(math.floor(x),math.floor(y)))
+        x = x + dx2
+        y = y + dy2
+        i = i + 1
+    return L
+
+def bresenham(x1,y1,x2,y2):
+    if x2<x1:
+        x1,y1,x2,y2 = x2,y2,x1,y1
+    x = x1
+    y = y1
+    dx = abs(x2-y1)
+    dy = abs(y2-y1)
+    if dx != 0:
+        if (dy/dx)>1:
+            x,y = y,x
+            m = 1 / (dy/dx)
+        else:
+            m = dy/dx
+    ne = 2 * dy - dx
+    L = []
+    i = 0
+    while i < dx:
+        if y1 == y2:
+            L.append(Cors(x,y1))
+        if x1 == x2:
+            L.append(Cors(x1,y))
+        else:
+            L.append(Cors(x,y))
+        while ne > 0:
+            y = y + 1
+            ne = ne - 2 * dx
+        x = x + 1
+        e = ne + 2 * dy
+        i = i + 1 
+
+def draw_pixels(surface,L,color):
+    for cors in L:
+        surface.set_at((cors.x, cors.y), color)
+    return True
 
 def draw_squares(surface,width,height,cell_size):
     #rects = np.zeros((int(width/cell_size),int(height/cell_size)))
@@ -65,23 +151,58 @@ def draw_at_positions(surface,squares,L,color):
     return True
 pygame.init()
 # Create the window, saving it to a variable.
-surface = pygame.display.set_mode((350, 250), pygame.RESIZABLE)
-pygame.display.set_caption("Example resizable window")
-
+surface = pygame.display.set_mode((1000, 600), pygame.RESIZABLE)
+pygame.display.set_caption("Practica 1")
+selectedAlgorithm = "Slope intercept"
 while True:
     surface.fill((255,255,255))
     height = surface.get_height()
     width = surface.get_width()
-    cell_size = 20
+    cell_size = 10
     point1 = width/2, height
     point2 = width/2, 0
     lineWidth = surface.get_width()/100
+
+    L = []
     # Draw a red rectangle that resizes with the window.
     squares = draw_squares(surface,width/2,height,cell_size)
-    L = slope_intercept(0,width/2,height,0)
-    draw_at_positions(surface,squares,L,BLUE)
-    NumberOfCellsPerLine = 20
     
+    
+    NumberOfCellsPerLine = 20
+
+    selectedPoints = []
+    
+    Buttons = [
+        Button(surface,Cors(width / 2 + 25,height * 1 / 5),180,30,"Slope intercept"),
+        Button(surface,Cors(width / 2 + 25,height * 2 / 5),180,30,"Digital Analyzer"),
+        Button(surface,Cors(width / 2 + 25,height * 3 / 5),180,30,"Bresenham")
+    ]
+    for ev in pygame.event.get():
+        if ev.type == pygame.MOUSEBUTTONUP:
+            x,y = pygame.mouse.get_pos()
+            cors = Cors(x,y)
+    
+
+    if (cors.x < width / 2):
+        selectedPoints.append(cors)
+
+    if (len(selectedPoints) == 2):
+        if selectedAlgorithm == "Slope Intercept":
+            L = slope_intercept(selectedPoints[0].x,selectedPoints[0].y,selectedPoints[1].x,selectedPoints[1].y)
+        if selectedAlgorithm == "Digital Analyzer":
+            L = digital_differential_analyzer(selectedPoints[0].x,selectedPoints[0].y,selectedPoints[1].x,selectedPoints[1].y)
+        if selectedAlgorithm == "Bresenham":
+            L = bresenham(selectedPoints[0].x,selectedPoints[0].y,selectedPoints[1].x,selectedPoints[1].y)
+        draw_at_positions(surface,squares,L,BLACK)
+        selectedPoints = []
+        L = []
+    for button in Buttons:
+        if button.isClicked(cors):
+            selectedAlgorithm = button.text
+        if selectedAlgorithm == button.text:
+            button.draw(GREEN)
+        else:
+            button.draw(BLACK)
 
     pygame.display.update()
     for event in pygame.event.get():
@@ -94,10 +215,13 @@ while True:
                 sys.exit()
 
         if event.type == pygame.VIDEORESIZE:
-            # There's some code to add back window content here.
-            surface = pygame.display.set_mode((event.w, event.h),
-                                              pygame.RESIZABLE)
+            width, height = event.size
+            if width < 1000:
+                width = 1000
+            if height < 600:
+                height = 600
+            screen = pygame.display.set_mode((width,height), HWSURFACE|DOUBLEBUF|RESIZABLE)
 
 
 
-     
+    
