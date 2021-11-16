f7868f1da90425fc465ea731dd263d0179e98b4f
PIRIS RUIZ <x442898@santanderglobaltech.com>
2021-11-16 16:21:27 +0100

    FALTA DIAGONAL

--- Práctica2/app/practica2.py
+++ Práctica2/app/practica2.py
@@ -17,16 +17,16 @@ canvas.place(x=0,y=0,height=height,width=width)
 img = PhotoImage(width=width, height=height)
 canvas.create_image((width/2, height/2), image=img, state="normal")
 
-sh_x,sh_y = 0.7,0.5
+sh_x,sh_y = 0.2,0.2
 scale_value = 1.2
-move_x,move_y=200,200
+move_x,move_y=100,-100
 x_point,y_point=100,100
 angle = 10
-rect_y_1 = 600
+rect_y_1 = 700
 rect_y_2 = 200
 
-rect_x_1 = 400
-rect_x_2 = 300
+rect_x_1 = 200
+rect_x_2 = 325
 
 def initialize_figure():
     x_list,y_list = [],[]
@@ -54,7 +54,7 @@ def delete_rect_y():
     a = canvas.delete("rect")
 
 def draw_rect_x():
-    a = canvas.create_rectangle(rect_x_1-300,rect_x_2,rect_x_1,rect_x_2,fill='black')
+    a = canvas.create_rectangle(rect_x_1,rect_x_2,rect_x_1+800,rect_x_2,fill='black',tags="rect")
 
 def delete_rect_x():
     a = canvas.delete("rect")
@@ -116,7 +116,7 @@ def draw_ref_x():
     x_list,y_list = initialize_figure()
     for i in range(len(x_list)):
         x = x_list[i]
-        y = int(y_list[i] - 2*(y_list[i])-rect_x_2)
+        y = int(y_list[i] - 2*(y_list[i]-rect_x_2))
         if (x < 0 or y < 0):
             tkinter.messagebox.showinfo("INVALID VALUE",  "Can't rotate such angle")  
             return False          
@@ -170,24 +170,24 @@ def main_loop():
     canvas.delete("default")
     canvas.create_text(int(width/2),10,fill="darkblue",font="Times 20 italic bold",
                         text="START",tags="default")
-    # window.after(1000,basicDraw)
-    # window.after(2000,draw_scale)
-    # window.after(3000,basicDraw)
-    # window.after(4000,draw_move)
-    # window.after(5000,basicDraw)
-    # window.after(6000,draw_rotate)
-    # window.after(7000,basicDraw)
-    # window.after(8000,draw_shearing)
-    # window.after(9000,basicDraw)
-    # window.after(9000,draw_rect_y)
-    # window.after(10000,draw_ref_y)
-    # window.after(11000,delete_rect_y)
     window.after(1000,basicDraw)
-    # window.after(2000,draw_rect_x)
-    # window.after(3000,draw_ref_x)
-    # window.after(4000,delete_rect_x)
-    # window.after(5000,basicDraw)
-    window.after(2000,main_loop)
+    window.after(2000,draw_scale)
+    window.after(3000,basicDraw)
+    window.after(4000,draw_move)
+    window.after(5000,basicDraw)
+    window.after(6000,draw_rotate)
+    window.after(7000,basicDraw)
+    window.after(8000,draw_shearing)
+    window.after(9000,basicDraw)
+    window.after(9000,draw_rect_y)
+    window.after(10000,draw_ref_y)
+    window.after(11000,delete_rect_y)
+    window.after(12000,basicDraw)
+    window.after(13000,draw_rect_x)
+    window.after(14000,draw_ref_x)
+    window.after(15000,delete_rect_x)
+    window.after(16000,basicDraw)
+    window.after(17000,main_loop)
 
 window.after(0,main_loop)
 window.mainloop()
